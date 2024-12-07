7ef84693f37fa38feb45b94ab6220764ddcb0486
AndK37 <146865715+AndK37@users.noreply.github.com>
2024-12-07 13:28:02 +0500

    header 1/2

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <my-header/>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
@@ -6,25 +7,27 @@
   <router-view/>
 </template>
 
+<script>
+import MyHeader from '@/components/MyHeader.vue'
+
+export default {
+ components: {
+  "my-header": MyHeader,
+ }
+}
+</script>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+}
+body {
+  margin: 0;
+  padding: 0;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
 nav a.router-link-exact-active {
-  color: #42b983;
+
 }
 </style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,141 @@
+<template>
+    <div style="width: 100%; background-color: #e1ede6;">
+        <div class="header">
+            <div class="logo">
+                <img src="../../public/img/header/logo.png" alt="" width="35px">
+                <span class="title">Ink. House</span>
+            </div>
+            <div class="nav">
+                <span class="nav-item">Репродукции</span>
+                <span class="nav-item">Новинки</span>
+                <span class="nav-item">О нас</span>
+                <img class="cart" src="../../public/img/header/cart.png" alt="" width="24px">
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+* {
+    font-family: 'Raleway';
+}
+
+.header {
+    max-width: 1110px;
+    height: 100px;
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: 5vw;
+    padding-right: 5vw;
+    
+    display: flex;
+    justify-content: space-between;
+}
+
+.logo {
+    display: flex;
+    align-items: center;
+}
+
+.title {
+    font-style: normal;
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 23px;
+    color: #598D66;
+}
+
+.nav {
+    display: flex;
+    align-items: center;
+    gap: 50px
+}
+
+.nav-item {
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 23px;
+    color: #2C2D35;
+}
+
+@media (max-width: 768px) {
+    .header {
+        max-width: 690px;
+
+    }
+
+    .title {
+        font-style: normal;
+        font-weight: 700;
+        font-size: 20px;
+        line-height: 23px;
+    }
+
+    .nav-item {
+        font-style: normal;
+        font-weight: 500;
+        font-size: 18px;
+        line-height: 21px;
+    }
+}
+
+@media (max-width: 576px) {
+    .header {
+        max-width: 510px;
+        flex-direction: column;
+        justify-content: space-evenly;
+    }
+
+    .logo {
+        align-self: center;
+    }
+
+    .nav {
+        align-self: center;
+        border-top: #c3d1c6 1px solid;
+        padding-top: 10px;
+    }
+
+    .title {
+        font-style: normal;
+        font-weight: 700;
+        font-size: 20px;
+        line-height: 23px;
+    }
+
+    .nav-item {
+        font-style: normal;
+        font-weight: 500;
+        font-size: 18px;
+        line-height: 21px;
+    }
+}
+
+@media (max-width: 320px) {
+    .header {
+        max-width: 290px;
+    }
+
+    .logo {
+
+    }
+
+    .nav {
+
+    }
+
+    .title {
+
+    }
+
+    .nav-item {
+
+    }
+}
+</style>
